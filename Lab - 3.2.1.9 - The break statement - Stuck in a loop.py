1add68cfab70f08421218b151e44e061a4d9d7bc
ErickArtola <99234108+ErickArtola@users.noreply.github.com>
2022-11-15 06:45:57 -0600

    Add files via upload

---
+++ Lab - 3.2.1.9 - The break statement - Stuck in a loop.py
@@ -0,0 +1,9 @@
+﻿secret_word = input("Hello, feeble human. Guess the secret word and I'll let you go:")
+
+while True:
+    if secret_word == "chupacabra":
+        break
+    else:
+        if secret_word != "chupacabra":
+            secret_word = input("Feeble human, your soul is mine. But try again if you want to escape: ")
+print ("You got lucky!")
